a0e65ca00b4a4eb9d1ec7c5b4ddd428b4f875be1
KOUSTAVSAMANTA <koustavsamanta007@gmail.com>
2024-04-24 00:13:59 +0530

    changed pushhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

--- frontend/src/hooks/useSendMessage.js
+++ frontend/src/hooks/useSendMessage.js
@@ -1,25 +1,51 @@
 import { useState } from "react";
 import useConversation from "../zustand/useConversation";
 import toast from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 
 const useSendMessage = () => {
 	const [loading, setLoading] = useState(false);
+	const { authUser } = useAuthContext();
 	const { messages, setMessages, selectedConversation } = useConversation();
 
 	const sendMessage = async (message) => {
 		setLoading(true);
 		try {
-			const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
-				method: "POST",
-				headers: {
-					"Content-Type": "application/json",
-				},
-				body: JSON.stringify({ message }),
-			});
-			const data = await res.json();
-			if (data.error) throw new Error(data.error);
+			console.log(message)
+			if(message.includes("@logious")){
+				console.log("AI",messages,selectedConversation._id,authUser._id)
 
-			setMessages([...messages, data]);
+				const res = await fetch('http://localhost:8000/predict/', {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ 
+						text: message.replace("@logious","logious"),
+						userID:selectedConversation._id+authUser._id
+				 }),
+				})
+				const data = await res.json()
+				console.log(data)
+				let msg ={createdAt:new Date().toISOString(),
+					message:data.response,
+					receiverId:selectedConversation._id,
+					senderId:authUser._id}
+				setMessages([...messages, msg]);
+			}else{
+				console.log("user")
+				const res = await fetch(`/api/messages/send/${selectedConversation._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ message }),
+				});
+				const data = await res.json();
+				if (data.error) throw new Error(data.error);
+	
+				setMessages([...messages, data]);
+			}
 		} catch (error) {
 			toast.error(error.message);
 		} finally {
