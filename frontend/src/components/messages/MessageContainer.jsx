46e496f7b6cbda376661de478f1e948cfb499252
PratyushSenSharma <pratyushsensharma@gmail.com>
2024-04-23 16:22:27 +0530

    asdas

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -4,6 +4,7 @@ import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import { useAuthContext } from "../../context/AuthContext";
+import "./msgcont.css"
 
 const MessageContainer = () => {
 	const { selectedConversation, setSelectedConversation } = useConversation();
@@ -14,14 +15,14 @@ const MessageContainer = () => {
 	}, [setSelectedConversation]);
 
 	return (
-		<div className='md:min-w-[450px] flex flex-col'>
+		<div className='md:min-w-[450px] flex flex-col wrapper '>
 			{!selectedConversation ? (
 				<NoChatSelected />
 			) : (
 				<>
 					{/* Header */}
-					<div className='bg-slate-500 px-4 py-2 mb-2'>
-						<span className='label-text'>To:</span>{" "}
+					<div className='bg-slate-500 px-4 py-2 mb-2 flex'>
+						<div className='w-12 rounded-full'><img src={selectedConversation.profilePic} alt='user avatar' className="h-8"/></div>{" "}
 						<span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
 					</div>
 					<Messages />
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
 	return (
-		<div className=' flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-[#161414] bg-clip-padding backdrop-filter backdrop-blur-lg '>
+		<div className=' flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-[#161414] bg-clip-padding backdrop-filter backdrop-blur-lg wrapper'>
 			<Sidebar />
 			<MessageContainer />
 		</div>
