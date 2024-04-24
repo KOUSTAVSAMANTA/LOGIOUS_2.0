2c8568ed21e4321711b76d733ae731c0bec2bad9
PratyushSenSharma <pratyushsensharma@gmail.com>
2024-04-24 11:43:43 +0530

    dssd

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -17,7 +17,8 @@ const Message = ({ message }) => {
 		<div className={`chat ${chatClassName}`}>
 			<div className='chat-image avatar'>
 				<div className='w-10 rounded-full'>
-					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+					<div className="w-10 rounded-full">
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} /></div>
 				</div>
 			</div>
 			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}>{message.message}</div>
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,19 +1,25 @@
+import { useAuthContext } from "../../context/AuthContext";
 import Conversations from "./Conversations";
 import LogoutButton from "./LogoutButton";
 import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
-	return (
+	const { authUser } = useAuthContext();
 
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-			{/* <div className='bg-slate-500 h-12 border-r border-slate-400 mb-2 flex'>
-				
-			</div> */}
-			<SearchInput />
-			<div className='divider px-3 '></div>
-			<Conversations  />
-			<LogoutButton />
-		</div>
+	return (
+		<div className="flex-col">
+			<div className='bg-slate-500 h-12 w-50 border-r border-slate-400 mb-2 p-4 flex justify-between '>
+
+			<img alt='Tailwind CSS chat bubble component w-30 h-30' src={authUser.profilePic} />
+				<LogoutButton />
+			</div>
+			<div className='border-r border-slate-500 p-4 flex flex-col'>
+
+				<SearchInput />
+				<div className='divider px-3 '></div>
+				<Conversations />
+
+			</div></div>
 	);
 };
 export default Sidebar;
