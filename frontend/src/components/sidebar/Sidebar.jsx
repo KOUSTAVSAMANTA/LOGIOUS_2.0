767473f5a655f4efacb4040367f63e9e4fe9414e
KOUSTAVSAMANTA <koustavsamanta007@gmail.com>
2024-04-25 13:25:17 +0530

    updated sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -8,16 +8,19 @@ const Sidebar = () => {
 
 	return (
 		<div className="flex-col border-r-1 shadow-right">
-		<div className='bg-slate-500 h-16 w-50 border-r border-slate-400 mb-2 p-4 flex justify-between items-center'>
-		  <img alt='Profile' src={authUser.profilePic} className="h-8 w-8 rounded-full" />
-		  <LogoutButton />
-		</div>
-		<div className='border-r border-slate-500 p-4 flex flex-col'>
-		  <SearchInput />
-		  <div className='divider px-3'></div>
-		  <Conversations />
-		</div>
-	  </div>
+  <div className='bg-slate-500 h-16 w-50 border-r border-slate-400 mb-2 p-4 flex justify-between items-center'>
+    <div className="flex items-center"> {/* Added a flex container for the image, name, and logout button */}
+      <img alt='Profile' src={authUser.profilePic} className="h-8 w-8 rounded-full mr-2" /> {/* Added mr-2 for spacing */}
+      <span className='text-gray-900 font-bold'>{authUser.fullName}</span>
+    </div>
+    <LogoutButton />
+  </div>
+  <div className='border-r border-slate-500 p-4 flex flex-col'>
+    <SearchInput />
+    <div className='divider px-3'></div>
+    <Conversations />
+  </div>
+</div>
 	);
 };
 export default Sidebar;
